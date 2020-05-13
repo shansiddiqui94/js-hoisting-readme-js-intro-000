37089e025f8af946fb8baafa032f41895ed9fd2b
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-05-13 19:45:10 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,11 @@
+var lyric = "maybe";
 function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+console.log("I just met you...");
+console.log("and this is crazy..");
+console.log("but here's my number..");
+console.log("so call me");
+return lyric;
+ lyric = "maybe";
 }
 
 
@@ -12,19 +13,23 @@ function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
+    thisIsCrazy();
+
+
   var thisIsCrazy = function (){
     console.log("hey!!!")
+function thisIsCrazy(){
+    console.log("hey!!!");
   }
 }
 
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
+console.log(name)
+var name = "Cricky";
 
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
+sayMy();
+  var name = "Kristin";
+function sayMy() {
+  console.log(name);
+}
 }
